f306bc6b2ffa77c64436f7f6ab72a115d1bef258
SANJAY <ks0077sanjay@gmail.com>
2023-06-25 06:10:49 +0530

    dev_ks0077

---
+++ GitVersionJeeraSpringMicroservices/src/test/java/com/GitVersionJeeraSpringMicroservices/main/GitVersionJeeraSpringMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GitVersionJeeraSpringMicroservices.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitVersionJeeraSpringMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
