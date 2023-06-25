b97a027ba5b20429c5725534351306b5b55ba32a
SANJAY <ks0077sanjay@gmail.com>
2023-06-25 06:36:54 +0530

    added controller class for Test ,application up and running

---
+++ GitVersionJeeraSpringMicroservices/src/main/java/com/GitVersionJeeraSpringMicroservices/main/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.GitVersionJeeraSpringMicroservices.main.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/home")
+	public String test() {
+		return "welcome";
+
+	}
+}
